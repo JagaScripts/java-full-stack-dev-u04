465118327fa4613379c9328dcca2cbb5e181d24d
Jose A González <jagascripts@gmail.com>
2022-04-06 20:27:40 +0200

    Ejercicio 4 agregado y terminado

---
+++ ud4/src/ejercicio4/Ej4.java
@@ -0,0 +1,45 @@
+package ejercicio4;
+/**
+ * 
+ * @author Jose Antonio Gonz�lez Alc�ntara
+ * 
+ * Clase de Java de la TA04-Java Basics Ejercicio4
+ */
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+		/*Escribe un programa Java que declare una variable entera N y asígnale una valor. 
+		A continuación escribe las instrucciones que realicen lo siguiente*/
+		
+		//Declaración de variable N
+		int n;
+		
+		//Asignación de un valor
+		n = 23;
+		
+		//Lo mostramos por pantalla
+		System.out.println("Valor inicial de N = " + n);
+		
+		//Incrementa N en 77
+		n = n + 77;
+		
+		//Lo mostramos por pantalla
+		System.out.println("N + 77 = " + n);
+		
+		//Decrementa en 3
+		n = n - 3;
+		
+		//Lo mostramos por pantalla
+		System.out.println("N - 3 = " + n);
+		
+		//Duplicar valor de N
+		n = n * 2;
+		
+		//Lo mostramos por pantalla
+		System.out.println("N * 2 = " + n);
+		
+		
+	}
+
+}
